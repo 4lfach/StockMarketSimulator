20e5048d5bbc725cdcec4467ebf61913a8fdaef1
4lfach <91556021+4lfach@users.noreply.github.com>
2022-03-14 23:47:51 +0600

    Add files via upload

---
+++ SimpleTrader.FinancialModellingPrepAPI/FinancialModellingPrepHttpClient.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json;
+using SimpleTrader.FinancialModellingPrepAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace SimpleTrader.FinancialModellingPrepAPI
+{
+    public class FinancialModellingPrepHttpClient
+    {
+        private readonly HttpClient _httpClient;
+        private readonly string _apiKey;
+        public FinancialModellingPrepHttpClient(HttpClient client, FinancialModellingPrepAPIKey apiKey)
+        {
+            _httpClient = client;
+            _apiKey = apiKey.Key;
+            _httpClient.BaseAddress = new Uri("https://financialmodelingprep.com/api/v3/");
+        }
+
+        public async Task<T> GetAsync<T>(string uri)
+        {
+            HttpResponseMessage responseMessage = await _httpClient.GetAsync($"{uri}?apikey={_apiKey}");
+            string jsonResponse = await responseMessage.Content.ReadAsStringAsync();
+
+            var result = JsonConvert.DeserializeObject<T>(jsonResponse);
+            return result;
+        }
+        public async Task<T> GetFromListAsync<T>(string uri)
+        {
+            HttpResponseMessage responseMessage = await _httpClient.GetAsync($"{uri}?apikey={_apiKey}");
+            string jsonResponse = await responseMessage.Content.ReadAsStringAsync();
+
+            var resultList = JsonConvert.DeserializeObject<List<T>>(jsonResponse);
+            return resultList[0];
+        }
+    }
+}
---
+++ SimpleTrader.FinancialModellingPrepAPI/Models/FinancialModellingPrepAPIKey.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleTrader.FinancialModellingPrepAPI.Models
+{
+    public class FinancialModellingPrepAPIKey
+    {
+        public string Key { get; set; }
+
+        public FinancialModellingPrepAPIKey(string key)
+        {
+            Key = key;
+        }
+    }
+}
